682252e1ce3978bb867988c47aefa14347d7cf3b
123 <123>
2026-05-15 10:36:33 -0300

    added script4

---
+++ js/script4.js
@@ -0,0 +1,13 @@
+let n1 = 10, n2 = 2;
+console.log(n1 + n2);
+console.log(n1 - n2);
+console.log(n1 * n2);
+console.log(n1 / n2);
+console.log(n1 % n2);
+console.log(n1 ** n2);
+
+let cont = 0;
+cont++
+console.log(cont);
+cont--
+console.log(cont);
