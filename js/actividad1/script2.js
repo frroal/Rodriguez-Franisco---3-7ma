004a4211a76b4b03ecb5c476199a796d53821a17
123 <123>
2026-05-15 11:55:07 -0300

    added script2 to exercise and started bubble sort

---
+++ js/actividad1/script2.js
@@ -0,0 +1,15 @@
+// 3 numeros
+
+let n1=5, n2=3, n3=4;
+
+console.log(n1 + " " + n2 + " " + n3)
+
+if(n1 > n2 && n1 > n3){
+    console.log(n1);
+}
+else if(n2 > n1 && n2 > n3){
+    console.log(n2);
+}
+else{
+    console.log(n3);
+}
