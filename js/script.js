f19ad3f8dc1b987ec0d9e0d3b371fe3538a6d7ec
123 <123>
2026-05-15 09:35:24 -0300

    Added table in script.js and added script2.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 // Esto es un mensaje normal
-console.log("Tamo chelo")
+console.log("Tamo chelo");
 // Esto es un mensaje de advertencia
-console.warn("CUIDADO")
+console.warn("CUIDADO");
 // Esto es un mensaje de error
-console.error("pincho")
+console.error("pincho");
+// Crear tabla
+console.table(["sa", "se",  "si"]);
+// Theme: Dark Pink Pro
---
+++ js/script2.js
@@ -0,0 +1,8 @@
+// Variables
+let name = "Martin";
+let edad = "64";
+
+console.log("nombre: " + name + " edad: " + edad);
+
+// Constantes
+const pi = 3.14159265359;
