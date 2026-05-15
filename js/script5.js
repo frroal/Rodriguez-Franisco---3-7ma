211c8d5d53f167455fde3df77170ad2ec5b85825
123 <123>
2026-05-15 10:45:02 -0300

    added script 5

---
+++ js/script5.js
@@ -0,0 +1,16 @@
+// Operadores logicos
+// and = &&
+console.log(true && true);    // true
+console.log(true && false);   // false
+console.log(false && true);   // false
+console.log(false && false);  // false
+
+// or == ||
+console.log(true || true);   // true
+console.log(true || false);  // true
+console.log(false || true);  // true
+console.log(false || false); // false
+
+// not = !
+console.log(!true);          // false
+console.log(!false);         // true
